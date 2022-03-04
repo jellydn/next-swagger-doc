20ffb4299f9ace576c6458935dd9661020000eaa
Huynh Duc Dung <dunghd.it@gmail.com>
2022-03-04 23:06:19 +0800

    feat: support schema folders

--- src/with-swagger.ts
+++ src/with-swagger.ts
@@ -5,16 +5,16 @@ import swaggerJsdoc, { Options } from 'swagger-jsdoc';
 
 type SwaggerOptions = Options & {
   apiFolder?: string;
-  schemaFolder?: string;
+  schemaFolders?: string[];
 };
 
 const defaultOptions: SwaggerOptions = {
   apiFolder: 'pages/api',
-  schemaFolder: 'models',
+  schemaFolders: [],
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Demo Api',
+      title: 'Next Swagger Doc Demo Api',
       version: '1.0',
     },
   },
@@ -24,36 +24,29 @@ const defaultOptions: SwaggerOptions = {
  * Create swagger JSON
  * @param options.openApiVersion Open API version {3.0.0}
  * @param options.apiFolder NextJS API folder {pages/api}
- * @param options.schemaFolder entity schema folder
+ * @param options.schemaFolders entity schema folders
  * @param options.title Title
  * @param options.version Version
  * @returns Swagger JSON Spec
  */
 export function createSwaggerSpec({
   apiFolder = 'pages/api',
-  schemaFolder = 'models',
+  schemaFolders = [],
   ...swaggerOptions
 }: SwaggerOptions = defaultOptions) {
-  const apiDirectory = join(process.cwd(), apiFolder);
-  const schemaDirectory = join(process.cwd(), schemaFolder);
-  const buildApiDirectory = join(process.cwd(), '.next/server', apiFolder);
-  const buildSchemaDirectory = join(
-    process.cwd(),
-    '.next/server',
-    schemaFolder
-  );
+  const scanFolders = [apiFolder, ...schemaFolders];
 
   const options: Options = {
-    apis: [
-      `${apiDirectory}/**/*.js`,
-      `${apiDirectory}/**/*.ts`,
-      `${apiDirectory}/**/*.tsx`,
-      `${schemaDirectory}/**/*.js`,
-      `${schemaDirectory}/**/*.ts`,
-      `${schemaDirectory}/**/*.tsx`,
-      `${buildApiDirectory}/**/*.js`,
-      `${buildSchemaDirectory}/**/*.js`,
-    ], // files containing annotations as above
+    apis: scanFolders.flatMap(apiFolder => {
+      const buildApiDirectory = join(process.cwd(), '.next/server', apiFolder);
+      const apiDirectory = join(process.cwd(), apiFolder);
+      return [
+        `${apiDirectory}/**/*.js`,
+        `${apiDirectory}/**/*.ts`,
+        `${apiDirectory}/**/*.tsx`,
+        `${buildApiDirectory}/**/*.js`,
+      ];
+    }), // files containing annotations as above
     ...swaggerOptions,
   };
 
@@ -64,13 +57,14 @@ export function createSwaggerSpec({
  * withSwagger middleware
  * @param options.openApiVersion Open API version {3.0.0}
  * @param options.apiFolder NextJS API folder {pages/api}
+ * @param options.schemaFolders entity schema folders
  * @param options.title Title
  * @param options.version Version
  * @returns
  */
 export function withSwagger({
   apiFolder = 'pages/api',
-  schemaFolder = 'models',
+  schemaFolders = [],
   ...swaggerOptions
 }: SwaggerOptions = defaultOptions) {
   return () => {
@@ -78,7 +72,7 @@ export function withSwagger({
       try {
         const swaggerSpec = createSwaggerSpec({
           apiFolder,
-          schemaFolder,
+          schemaFolders,
           ...swaggerOptions,
         });
         res.status(200).send(swaggerSpec);

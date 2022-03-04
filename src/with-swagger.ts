23273b03d78b2b25b9a11f45d5e2e47c8ae98ba9
Rizal <fahrizal.maruf@gmail.com>
2022-03-04 11:18:46 +0800

    add schemaFolder options to get component schema spec

--- src/with-swagger.ts
+++ src/with-swagger.ts
@@ -5,10 +5,12 @@ import swaggerJsdoc, { Options } from 'swagger-jsdoc';
 
 type SwaggerOptions = Options & {
   apiFolder?: string;
+  schemaFolder?: string;
 };
 
 const defaultOptions: SwaggerOptions = {
   apiFolder: 'pages/api',
+  schemaFolder: 'models',
   definition: {
     openapi: '3.0.0',
     info: {
@@ -22,25 +24,37 @@ const defaultOptions: SwaggerOptions = {
  * Create swagger JSON
  * @param options.openApiVersion Open API version {3.0.0}
  * @param options.apiFolder NextJS API folder {pages/api}
+ * @param options.schemaFolder entity schema folder
  * @param options.title Title
  * @param options.version Version
  * @returns Swagger JSON Spec
  */
 export function createSwaggerSpec({
   apiFolder = 'pages/api',
+  schemaFolder = 'models',
   ...swaggerOptions
 }: SwaggerOptions = defaultOptions) {
   const apiDirectory = join(process.cwd(), apiFolder);
+  const schemaDirectory = join(process.cwd(), schemaFolder);
   const buildApiDirectory = join(process.cwd(), '.next/server', apiFolder);
+  const buildSchemaDirectory = join(
+    process.cwd(),
+    '.next/server',
+    schemaFolder
+  );
 
   const options: Options = {
-    ...swaggerOptions,
     apis: [
       `${apiDirectory}/**/*.js`,
       `${apiDirectory}/**/*.ts`,
       `${apiDirectory}/**/*.tsx`,
+      `${schemaDirectory}/**/*.js`,
+      `${schemaDirectory}/**/*.ts`,
+      `${schemaDirectory}/**/*.tsx`,
       `${buildApiDirectory}/**/*.js`,
+      `${buildSchemaDirectory}/**/*.js`,
     ], // files containing annotations as above
+    ...swaggerOptions,
   };
 
   return swaggerJsdoc(options);
@@ -56,6 +70,7 @@ export function createSwaggerSpec({
  */
 export function withSwagger({
   apiFolder = 'pages/api',
+  schemaFolder = 'models',
   ...swaggerOptions
 }: SwaggerOptions = defaultOptions) {
   return () => {
@@ -63,6 +78,7 @@ export function withSwagger({
       try {
         const swaggerSpec = createSwaggerSpec({
           apiFolder,
+          schemaFolder,
           ...swaggerOptions,
         });
         res.status(200).send(swaggerSpec);

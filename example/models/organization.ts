0d9d8d91f31601aaae0de08dd1844bcd8dde388d
Rizal <fahrizal.maruf@gmail.com>
2022-03-04 11:39:02 +0800

    add schema spec in empty model file

---
+++ example/models/organization.ts
@@ -0,0 +1,25 @@
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Organization:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: string
+ *          format: uuid
+ *        kode:
+ *          type: string
+ *        nama:
+ *          type: string
+ *        parent:
+ *          type: string
+ *        createdAt:
+ *          type: string
+ *          format: date-time
+ *        updatedAt:
+ *          type: string
+ *          format: date-time
+ */
+
+export default {};

590cc0a02f957dabce0a14060794ed84d9174195
Huynh Duc Dung <dunghd.it@gmail.com>
2023-06-28 23:55:43 +0800

    chore: remove example in favor of 2 other apps

--- examples/next13-example/src/app/api-doc/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getApiDocs } from '../../lib/swagger';
-import ReactSwagger from './ReactSwagger';
-
-async function ApiDocPage() {
-  const spec = await getApiDocs();
-  return <ReactSwagger spec={spec} />;
-}
-
-export default ApiDocPage;

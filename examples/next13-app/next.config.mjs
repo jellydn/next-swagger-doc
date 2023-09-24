74ba7423eb51084a155308d45538344172a0e58a
Huynh Duc Dung <dunghd.it@gmail.com>
2023-09-24 12:20:09 +0800

    fix(example): add reference to swagger ui issue

--- examples/next13-app/next.config.mjs
+++ examples/next13-app/next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
+  // Transpile Swagger UI React https://github.com/swagger-api/swagger-ui/issues/8245
+  transpilePackages: [
+    "react-syntax-highlighter",
+    "swagger-client",
+    "swagger-ui-react",
+  ],
 }
 
 export default nextConfig

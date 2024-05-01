acb32c75b512d44a1c1b82bba7a78ed40d1e7ff2
Huynh Duc Dung <dunghd.it@gmail.com>
2024-05-01 13:14:51 +0800

    chore: update deps

--- examples/next13-simple/.prettierrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  printWidth: 80,
-  semi: true,
-  singleQuote: true,
-  trailingComma: 'es5',
-  importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-};
